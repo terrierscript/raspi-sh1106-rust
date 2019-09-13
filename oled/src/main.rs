851bb42497fba1c76b970cb62a463855f6e8a8f4
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-13 17:21:13 +0900

    append

--- oled/src/main.rs
+++ oled/src/main.rs
@@ -1,3 +1,76 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+extern crate cortex_m;
+extern crate cortex_m_rt as rt;
+extern crate panic_semihosting;
+extern crate stm32f1xx_hal as hal;
+
+use cortex_m_rt::ExceptionFrame;
+use cortex_m_rt::{entry, exception};
+use embedded_graphics::fonts::Font6x8;
+use embedded_graphics::prelude::*;
+use hal::i2c::{BlockingI2c, DutyCycle, Mode};
+use hal::prelude::*;
+use hal::stm32;
+use sh1106::prelude::*;
+use sh1106::Builder;
+
+#[entry]
+fn main() -> ! {
+  let dp = stm32::Peripherals::take().unwrap();
+
+  let mut flash = dp.FLASH.constrain();
+  let mut rcc = dp.RCC.constrain();
+
+  let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+  let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+  let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+
+  let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+  let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+  let i2c = BlockingI2c::i2c1(
+    dp.I2C1,
+    (scl, sda),
+    &mut afio.mapr,
+    Mode::Fast {
+      frequency: 400_000,
+      duty_cycle: DutyCycle::Ratio2to1,
+    },
+    clocks,
+    &mut rcc.apb1,
+    1000,
+    10,
+    1000,
+    1000,
+  );
+
+  let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+
+  disp.init().unwrap();
+  disp.flush().unwrap();
+
+  disp.draw(
+    Font6x8::render_str("Hello world!")
+      .stroke_width(1)
+      .into_iter(),
+  );
+  disp.draw(
+    Font6x8::render_str("Hello Rust!")
+      .stroke_width(1)
+      .translate(Point::new(0, 16))
+      .into_iter(),
+  );
+
+  disp.flush().unwrap();
+
+  loop {}
+}
+
+#[exception]
+fn HardFault(ef: &ExceptionFrame) -> ! {
+  panic!("{:#?}", ef);
 }
