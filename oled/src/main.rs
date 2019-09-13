2c6cdc0af5cb5be8c8dabf0ada8c1b927b3c3dca
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-13 17:59:32 +0900

    add

--- oled/src/main.rs
+++ oled/src/main.rs
@@ -1,3 +1,20 @@
+//! Print "Hello world!" with "Hello rust!" underneath. Uses the `embedded_graphics` crate to draw
+//! the text with a 6x8 pixel font.
+//!
+//! This example is for the STM32F103 "Blue Pill" board using I2C1.
+//!
+//! Wiring connections are as follows for a CRIUS-branded display:
+//!
+//! ```
+//!      Display -> Blue Pill
+//! (black)  GND -> GND
+//! (red)    +5V -> VCC
+//! (yellow) SDA -> PB9
+//! (green)  SCL -> PB8
+//! ```
+//!
+//! Run on a Blue Pill with `cargo run --example text`.
+
 #![no_std]
 #![no_main]
 
@@ -18,59 +35,59 @@ use sh1106::Builder;
 
 #[entry]
 fn main() -> ! {
-  let dp = stm32::Peripherals::take().unwrap();
+    let dp = stm32::Peripherals::take().unwrap();
 
-  let mut flash = dp.FLASH.constrain();
-  let mut rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
 
-  let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-  let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
 
-  let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
 
-  let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
-  let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
 
-  let i2c = BlockingI2c::i2c1(
-    dp.I2C1,
-    (scl, sda),
-    &mut afio.mapr,
-    Mode::Fast {
-      frequency: 400_000,
-      duty_cycle: DutyCycle::Ratio2to1,
-    },
-    clocks,
-    &mut rcc.apb1,
-    1000,
-    10,
-    1000,
-    1000,
-  );
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Fast {
+            frequency: 400_000,
+            duty_cycle: DutyCycle::Ratio2to1,
+        },
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        10,
+        1000,
+        1000,
+    );
 
-  let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+    let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
 
-  disp.init().unwrap();
-  disp.flush().unwrap();
+    disp.init().unwrap();
+    disp.flush().unwrap();
 
-  disp.draw(
-    Font6x8::render_str("Hello world!")
-      .stroke_width(1)
-      .into_iter(),
-  );
-  disp.draw(
-    Font6x8::render_str("Hello Rust!")
-      .stroke_width(1)
-      .translate(Point::new(0, 16))
-      .into_iter(),
-  );
+    disp.draw(
+        Font6x8::render_str("Hello world!")
+            .stroke_width(1)
+            .into_iter(),
+    );
+    disp.draw(
+        Font6x8::render_str("Hello Rust!")
+            .stroke_width(1)
+            .translate(Point::new(0, 16))
+            .into_iter(),
+    );
 
-  disp.flush().unwrap();
+    disp.flush().unwrap();
 
-  loop {}
+    loop {}
 }
 
 #[exception]
 fn HardFault(ef: &ExceptionFrame) -> ! {
-  panic!("{:#?}", ef);
+    panic!("{:#?}", ef);
 }
