b2815ce92a73ef05815afb4ee089a1c84c2b1032
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-15 00:23:25 +0900

    add

--- oled/src/main.rs
+++ oled/src/main.rs
@@ -1,98 +1,71 @@
-// //! Print "Hello world!" with "Hello rust!" underneath. Uses the `embedded_graphics` crate to draw
-// //! the text with a 6x8 pixel font.
-// //!
-// //! This example is for the STM32F103 "Blue Pill" board using I2C1.
-// //!
-// //! Wiring connections are as follows for a CRIUS-branded display:
-// //!
-// //! ```
-// //!      Display -> Blue Pill
-// //! (black)  GND -> GND
-// //! (red)    +5V -> VCC
-// //! (yellow) SDA -> PB9
-// //! (green)  SCL -> PB8
-// //! ```
-// //!
-// //! Run on a Blue Pill with `cargo run --example text`.
 
-// #![no_std]
-// #![no_main]
+#![no_std]
+#![no_main]
 
-// extern crate cortex_m;
-// extern crate cortex_m_rt as rt;
-// extern crate panic_semihosting;
+extern crate cortex_m;
+extern crate cortex_m_rt as rt;
+extern crate panic_semihosting;
 // extern crate stm32f1xx_hal as hal;
 
-// use cortex_m_rt::ExceptionFrame;
-// use cortex_m_rt::{entry, exception};
-// use embedded_graphics::fonts::Font6x8;
-// use embedded_graphics::prelude::*;
+
+use cortex_m_rt::ExceptionFrame;
+use cortex_m_rt::{entry, exception};
+use embedded_graphics::{image::Image, pixelcolor::BinaryColor, prelude::*};
+
 // use hal::i2c::{BlockingI2c, DutyCycle, Mode};
 // use hal::prelude::*;
 // use hal::stm32;
-// use sh1106::prelude::*;
-// use sh1106::Builder;
+use sh1106::prelude::*;
+use sh1106::Builder;
 
-// #[entry]
-// fn main() -> ! {
-//     let dp = stm32::Peripherals::take().unwrap();
+#[entry]
+fn main() -> ! {
+    let dp = stm32::Peripherals::take().unwrap();
 
-//     let mut flash = dp.FLASH.constrain();
-//     let mut rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
 
-//     let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-//     let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
 
-//     let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
 
-//     let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
-//     let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
 
-//     let i2c = BlockingI2c::i2c1(
-//         dp.I2C1,
-//         (scl, sda),
-//         &mut afio.mapr,
-//         Mode::Fast {
-//             frequency: 400_000,
-//             duty_cycle: DutyCycle::Ratio2to1,
-//         },
-//         clocks,
-//         &mut rcc.apb1,
-//         1000,
-//         10,
-//         1000,
-//         1000,
-//     );
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Fast {
+            frequency: 400_000,
+            duty_cycle: DutyCycle::Ratio2to1,
+        },
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        10,
+        1000,
+        1000,
+    );
 
-//     let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+    let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
 
-//     disp.init().unwrap();
-//     disp.flush().unwrap();
+    disp.init().unwrap();
+    disp.flush().unwrap();
 
-//     disp.draw(
-//         Font6x8::render_str("Hello world!")
-//             .stroke_width(1)
-//             .into_iter(),
-//     );
-//     disp.draw(
-//         Font6x8::render_str("Hello Rust!")
-//             .stroke_width(1)
-//             .translate(Point::new(0, 16))
-//             .into_iter(),
-//     );
+    let im: Image<BinaryColor> =
+        Image::new(include_bytes!("./rust.raw"), 64, 64).translate(Point::new(32, 0));
 
-//     disp.flush().unwrap();
+    disp.draw(im.into_iter());
 
-//     loop {}
-// }
+    disp.flush().unwrap();
 
-// #[exception]
-// fn HardFault(ef: &ExceptionFrame) -> ! {
-//     panic!("{:#?}", ef);
-// }
+    loop {}
+}
 
-pub fn main() {
-    println!("cargo:rustc-linker=arm-linux-gnueabihf-gcc");
-    println!("cargo:rustc-flags=static=link-arg=-march=armv6");
+#[exception]
+fn HardFault(ef: &ExceptionFrame) -> ! {
+    panic!("{:#?}", ef);
 }
