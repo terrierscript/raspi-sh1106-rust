f7bbfc5605084881c2c0a8bcd306bdb2e2c3e83f
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-15 12:50:33 +0900

    add

--- js/daemon.js
+++ js/daemon.js
@@ -9,7 +9,7 @@ setInterval(() => {
   const lines = [os.arch(), os.cpus()[0].times.user]
 
   // Draw the current time at [1, 1] with a size of 2
-  lines.map(l, (i) => {
+  lines.map((l, i) => {
     device.canvas.text(i * 10, 1, l.toString(), 1)
   })
 
