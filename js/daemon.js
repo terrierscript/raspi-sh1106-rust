6bf94a23ef98177bdd0b3a64bdb8ce8e8a4f1b90
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-15 12:52:15 +0900

    add

--- js/daemon.js
+++ js/daemon.js
@@ -6,7 +6,11 @@ const device = new SH1106()
 setInterval(() => {
   // Clear the canvas
   device.canvas.clear()
-  const lines = [os.arch(), os.cpus()[0].times.user]
+  const lines = [
+    `arch: ${os.arch()}`,
+    `cpu: ${os.cpus()[0].times.user}`,
+    `mem: ${os.freemem()}`
+  ]
 
   // Draw the current time at [1, 1] with a size of 2
   lines.map((l, i) => {
