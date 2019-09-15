66a68efc1ce3f8b4c5f8a7e1069f382c71d3aee0
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-15 12:49:10 +0900

    fix

--- js/daemon.js
+++ js/daemon.js
@@ -6,13 +6,12 @@ const device = new SH1106()
 setInterval(() => {
   // Clear the canvas
   device.canvas.clear()
+  const lines = [os.arch(), cpus[0].times.user]
 
   // Draw the current time at [1, 1] with a size of 2
-  device.canvas.text(1, 1, os.arch(), 1)
-  const cpus = os.cpus()
-  if (cpus[0].times) {
-    device.canvas.text(10, 1, cpus[0].times.user, 1)
-  }
+  lines.map(l, (i) => {
+    device.canvas.text(i * 10, 1, l.toString(), 1)
+  })
 
   // Update the display
   device.refresh()
