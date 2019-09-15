7a6cd88cc523f7305b319bd243d8b9230d4a7055
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-15 16:52:47 +0900

    add

--- oled/src/main.rs
+++ oled/src/main.rs
@@ -1,4 +1,3 @@
-  
 //! Print "Hello world!" with "Hello rust!" underneath. Uses the `embedded_graphics` crate to draw
 //! the text with a 6x8 pixel font.
 //!
@@ -16,38 +15,50 @@
 //!
 //! Run on a Blue Pill with `cargo run --example text`.
 
-#![no_std]
-#![no_main]
+// #![no_std]
+// #![no_main]
 
-// extern crate linux_embedded_hal as hal;
-// extern crate embedded_graphics;
-// extern crate sh1106;
+extern crate linux_embedded_hal as hal;
+extern crate embedded_graphics;
+extern crate sh1106;
 
 // use hal::I2cdev;
-// use embedded_graphics::prelude::*;
+use hal::Spidev;
+use hal::Pin;
+
+use embedded_graphics::prelude::*;
 // use embedded_graphics::primitives::{Line};
 use sh1106::prelude::*;
 use sh1106::Builder;
 
+// #![main]
 fn main() {
-
+    println!("start");
     // let i2c = I2cdev::new("/dev/i2c-1").unwrap();
+    let spi = Spidev::open("/dev/spidev0.0").unwrap_or_else(|e| -> {
+        println!("cannnot open");
+        println!("{:?}",e);
+    });
 
-    let b = Builder::new();
-    // let mut disp: GraphicsMode<
-    //     // I2cInterface<hal::I2cdev>
-    //     _
-    // > = Builder::new()
-    // .connect_i2c(i2c).into();
+    // let b = Builder::new();
+    let dc = Pin::new(24);
+    let mut disp: GraphicsMode<_> = Builder::new().connect_spi(spi, dc).into();
 
-    // disp.init().unwrap();
-    // disp.flush().unwrap();
+    disp.init().unwrap_or_else(|e| {
+        println!("not initialized");
+        println!("{:?}", e);
+    });
+    disp.flush().unwrap_or_else(|e| {
+        println!("not flush");
+        println!("{:?}", e);
+    });
     
-    // disp.set_pixel(10, 10, 1);
-    // disp.set_pixel(10, 11, 1);
-    // disp.set_pixel(10, 12, 1);
+    disp.set_pixel(10, 10, 1);
+    disp.set_pixel(10, 11, 1);
+    disp.set_pixel(10, 12, 1);
 
-    // disp.flush().unwrap();
+    println!("end");
+    disp.flush().unwrap();
 }
 
 // #[exception]
@@ -55,7 +66,7 @@ fn main() {
 //     panic!("{:#?}", ef);
 // }
 
-#[panic_handler]
-fn my_panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
+// #[panic_handler]
+// fn my_panic(_info: &core::panic::PanicInfo) -> ! {
+//     loop {}
+// }
