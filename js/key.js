d36b31799e7251de9d1ca8fca0665ce9f7a37ea9
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-16 15:31:06 +0900

    add

--- js/key.js
+++ js/key.js
@@ -1,39 +1,30 @@
 const { SH1106 } = require("sh1106")
 
 const rpio = require("rpio")
-const {LOW} = rpio
+const { LOW } = rpio
 
 const device = new SH1106()
 
 console.log("start")
-const buttons = [
-  21,
-  20,
-  16,
-  6,
-  19,
-  5,
-  26,
-]
-buttons.map(b => {
+const buttons = [21, 20, 16, 6, 19, 5, 26]
+buttons.map((b) => {
   console.log(b)
-  rpio.open(b, rpio.INPUT,rpio.PULL_UP);
+  rpio.open(b, rpio.INPUT, rpio.PULL_UP)
 })
 // rpio.open(20, rpio.INPUT, rpio.PULL_UP);
 
-
-// buttons.map(b => {
-  
-  // rpio.poll(21, cb)
-// })
-setInterval(() => {
-  buttons.map(b => {
-    const l = rpio.read(b)
-    // console.log(b, l)
-    if (l === LOW) {
-      console.log('Foo', b); // Called when button is pressed
-    }
-  })
-}, 10);
-
-console.log("end")
+module.exports = (onPressKey) => {
+  let buffer
+  setInterval(() => {
+    buttons.map((b) => {
+      const l = rpio.read(b)
+      // console.log(b, l)
+      if (l === LOW) {
+        if (buffer != l) {
+          buffer = l
+          onPressKey(l)
+        }
+      }
+    })
+  }, 10)
+}
