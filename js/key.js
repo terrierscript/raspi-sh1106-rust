5cee0736d7d6dc2a4e2a0fdbb653637da12c77af
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-16 13:58:54 +0900

    add

--- js/key.js
+++ js/key.js
@@ -5,18 +5,18 @@ const {LOW} = rpio
 console.log("start")
 const buttons = [
   21,
-  // 20,
-  // 16,
-  // 6,
-  // 19,
-  // 5,
-  // 26,
+  20,
+  16,
+  6,
+  19,
+  5,
+  26,
 ]
-// buttons.map(b => {
-//   console.log(b)
-// })
-rpio.open(21, rpio.INPUT,rpio.PULL_UP);
-rpio.open(20, rpio.INPUT, rpio.PULL_DOWN);
+buttons.map(b => {
+  console.log(b)
+  rpio.open(b, rpio.INPUT,rpio.PULL_UP);
+})
+// rpio.open(20, rpio.INPUT, rpio.PULL_UP);
 
 const cb = (cbpin) => {
   console.log("button",cbpin)
@@ -28,9 +28,13 @@ const cb = (cbpin) => {
 // })
 setInterval(() => {
   console.log(rpio.read(21))
-  if (rpio.read(21) === LOW) {
-    console.log('Foo'); // Called when button is pressed
-  }
+  buttons.map(b => {
+    const l = rpio.read(b)
+    console.log(b, l)
+    if (l === LOW) {
+      console.log('Foo', b); // Called when button is pressed
+    }
+  })
 }, 100);
 
 console.log("end")
