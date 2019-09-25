9ec05c9140fb745885eef648b687227dda93f339
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-25 10:44:47 +0900

    cleanup

--- key-event/src/lib.rs
+++ key-event/src/lib.rs
@@ -1,15 +1,12 @@
 extern crate rppal;
 extern crate sh1106;
 
-use std::sync::mpsc::Sender;
 use rppal::gpio::{Gpio, InputPin};
 
 // use std::borrow::Borrow;
 // use std::collections::HashMap;
 mod keymap;
 use std::sync::mpsc::channel;
-use std::thread;
-use std::thread::spawn;
 
 use crate::keymap::Keymap;
 
@@ -25,79 +22,27 @@ where
 
     let gpio = Gpio::new().expect("Failed Gpio::new");
     let keymap = Keymap::new();
-    for pid in keymap.keys() {
-        let mut p = gpio.get(*pid).expect("get pin").into_input_pullup();
-        let kk = Keymap::new();
-        let setting = kk.get_setting(*pid).expect("invalid pin");
-        let pp = pid.clone();
-        let tt = tx.clone();
-        println!("pp {:?}", pp);
-        // let name = String::from(&setting.name);
-        p.set_async_interrupt(setting.trigger, move |lv| {
-            println!("before");
-            tt.send(pp).expect("send tx");
-            // key_event(pp); //, lv as i8);
+    let _pins: Vec<InputPin> = keymap
+        .keys()
+        .into_iter()
+        .map(move |pid| {
+            let mut p = gpio.get(*pid).expect("get pin").into_input_pullup();
+            let kk = Keymap::new();
+            let setting = kk.get_setting(*pid).expect("invalid pin");
+            let pp = pid.clone();
+            let tt = tx.clone();
+            p.set_async_interrupt(setting.trigger, move |lv| {
+                tt.send(pp).expect("send tx");
+            })
+            .expect("invalid set async interrupt");
+            p
         })
-        .expect("invalid set async interrupt");
+        .collect();
+    loop{
+        rx.try_recv().and_then(|r| {
+            // println!("{:?}",r);
+            key_event(r);
+            Ok(r)
+        }).ok();
     }
-    
-    // rx.recv().and_then(|r| {
-    //     println!("{:?}",r);
-    //     Ok(r)
-    // }).expect("receive failed");
-
 }
-
-// pub fn hook_keyevent2<F>(sender: Sender<u8>) 
-
-// {
-//     let gpio = Gpio::new().expect("Failed Gpio::new");
-//     let keymap = Keymap::new();
-//     let pins: Vec<_> = keymap
-//         .keys()
-//         .into_iter()
-//         .map(move |pid| {
-//             let mut p = gpio.get(*pid).expect("get pin").into_input_pullup();
-//             let kk = Keymap::new();
-//             let setting = kk.get_setting(*pid).expect("invalid pin");
-//             let pp = pid.clone();
-//             p.set_async_interrupt(setting.trigger, move |lv| {
-//                 thread::spawn( || {
-//                     sender.send(pp);
-//                 });
-//                 // return pp;
-//                 // key_event(pp); //, lv as i8);
-//             })
-//             .expect("");
-//         })
-//         .collect();
-//     // pins
-// }
-
-// pub fn pin_to_pin<'a>() -> &'static [&'a InputPin]{
-//     // gpio.poll_interrupts(pins: &[&'a InputPin], reset: bool, timeout: Option<Duration>)
-    
-//     let ff = || {
-//         // key_event()
-//     };
-//     let gpio = Gpio::new().expect("Failed Gpio::new");
-//     let keymap = Keymap::new();
-//     let pins: Vec<_> = keymap
-//         .keys()
-//         .into_iter()
-//         .map(move |pid| {
-//             let mut p = gpio.get(*pid).expect("get pin").into_input_pullup();
-//             &p
-//         })
-//         .collect();
-//     // let pp : Vec<InputPin> = pins.map(|p| &p).collect();
-//     return pins.as_slice();
-// }
-
-// pub fn hook_keyevent<C>(key_event: C)
-// where
-//     C: FnMut(u8) + Send + 'static ,
-// {
-//     let pins = get_pins(key_event);
-//     loop {}
-// }
--- main/src/main.rs
+++ main/src/main.rs
@@ -2,30 +2,21 @@ extern crate embedded_graphics;
 extern crate linux_embedded_hal as hal;
 extern crate sh1106;
 
-use std::sync::mpsc::channel;
-use std::sync::Mutex;
 use canvas::Canvas;
 use key_event::hook_keyevent;
 
 use random_pos::random;
 use sh1106::prelude::*;
-use spidev_sh1106::display::Display;
 use spidev_sh1106::SpidevSH1106;
-use std::sync::Arc;
-// use embedded_graphics::text_12x16;
 
 fn main() {
     spi2();
 }
 
 fn spi2() {
-    let d = Display::new();
-    // let dd = d.gen_display();
-    // let sd = SpidevSH1106::new();
-    let mut cnv = Arc::new(Mutex::new(Canvas::new()));
+    let mut canvas = Canvas::new();
     let mut disp: GraphicsMode<_> = SpidevSH1106::gen_display();
-    let (tx, rx) = channel();
-
+    
     SpidevSH1106::reset(&mut disp).expect("cannot reset");
     
 
@@ -37,29 +28,15 @@ fn spi2() {
         disp.set_pixel(random(128), random(64), 1);
     }
 
-    disp.flush().expect("cannot flushed");
-    let dispMutex = Mutex::new(disp);
-    // let b = Rc::new(disp);
-    // set_keys();
     hook_keyevent(
-        // let event_cb =
          move |pin: u8| {
-            tx.send(pin);
-            // let mut lock = cnv.try_lock();
-            // let mut lock2 = dispMutex.try_lock();
-            // if let Ok(ref mut canvas) = lock {
-            // if let Ok(ref mut disp) = lock2 {
-            //         println!("{:?}", pin);
-            //         let disp: GraphicsMode<_> = SpidevSH1106::gen_display();
-            //         let keyenum = keyenum::from_u8(pin).expect("invalid pin");
-            //         canvas.move_char(keyenum);
-            //         let d2 = canvas.draw_char(disp);
-            //         d2.flush().expect("fllll");
-            //     }
-            // }
-        }, // event_cb
+                    let disp: GraphicsMode<_> = SpidevSH1106::gen_display();
+                    let keyenum = keyenum::from_u8(pin).expect("invalid pin");
+                    canvas.move_char(keyenum);
+                    let mut d2 = canvas.draw_char(disp);
+                    d2.flush().expect("fllll");
+        }
     );
-    loop {}
     println!("hook");
     println!("end");
 }
