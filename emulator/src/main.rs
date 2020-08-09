6c329b1155a56ace15f3a925f69a4de92cfc5745
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-08-09 12:43:42 +0900

    add

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -1,25 +1,29 @@
 use canvas::Canvas;
+use embedded_graphics::pixelcolor::BinaryColor;
 use embedded_graphics::prelude::*;
-use embedded_graphics_simulator::{DisplayBuilder, DisplayTheme};
-
+use embedded_graphics_simulator::{
+    BinaryColorTheme, SimulatorDisplay, SimulatorEvent, Window, OutputSettingsBuilder,
+};
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let mut display = DisplayBuilder::new()
-        .theme(DisplayTheme::OledBlue)
-        .size(128, 64)
-        .build();
-    let mut canvas = Canvas::new();
-    let d2 = canvas.draw_char(display);
-    // d2.flush().expect("fllll");
-    loop {
-        let end = display.run_once();
+    let mut display = SimulatorDisplay::new(
+        Size::new(128, 64), 
+    );
+        // .theme(DisplayTheme::OledBlue)
+        // .size(128, 64)
+        // .build();
+    // let mut canvas = Canvas::new();
+    // let d2 = canvas.draw_char(display);
+    // // d2.flush().expect("fllll");
+    // loop {
+    //     let end = display.run_once();
 
-        if end {
-            break;
-        }
+    //     if end {
+    //         break;
+    //     }
 
-        thread::sleep(Duration::from_millis(200));
-    }
+    //     thread::sleep(Duration::from_millis(200));
+    // }
 }
