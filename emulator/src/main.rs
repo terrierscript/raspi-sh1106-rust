cf7f28c27f1c7a3e61a60dab3a7e9899edbea4cb
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-08-09 12:25:51 +0900

    wip

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -1,3 +1,25 @@
+use canvas::Canvas;
+use embedded_graphics::prelude::*;
+use embedded_graphics_simulator::{DisplayBuilder, DisplayTheme};
+
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut display = DisplayBuilder::new()
+        .theme(DisplayTheme::OledBlue)
+        .size(128, 64)
+        .build();
+    let mut canvas = Canvas::new();
+    let d2 = canvas.draw_char(display);
+    // d2.flush().expect("fllll");
+    loop {
+        let end = display.run_once();
+
+        if end {
+            break;
+        }
+
+        thread::sleep(Duration::from_millis(200));
+    }
 }
