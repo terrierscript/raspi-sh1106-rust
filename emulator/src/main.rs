a1612231628d32e907dbb55d28983c9a1c767a8f
terrierscript <13282103+terrierscript@users.noreply.github.com>
2020-08-09 21:08:11 +0900

    add

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -8,22 +8,30 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let mut display = SimulatorDisplay::new(
+    let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(
         Size::new(128, 64), 
     );
+    let setting = OutputSettingsBuilder::new().theme(BinaryColorTheme::OledBlue).build();
+
         // .theme(DisplayTheme::OledBlue)
         // .size(128, 64)
         // .build();
-    // let mut canvas = Canvas::new();
-    // let d2 = canvas.draw_char(display);
+    let mut window = Window::new("test", &setting);
+    let mut canvas = Canvas::new();
+    let d2 = canvas.draw_char(display);
     // // d2.flush().expect("fllll");
-    // loop {
-    //     let end = display.run_once();
+    'running: loop {
+        window.update(&d2);
+        for event in window.events() {
+            match event {   
+                SimulatorEvent::MouseButtonUp { point, .. } => {
+                    println!("Click event at ({}, {})", point.x, point.y);
+                }
+                SimulatorEvent::Quit => break 'running,
+                _ => {}
+            }
 
-    //     if end {
-    //         break;
-    //     }
-
-    //     thread::sleep(Duration::from_millis(200));
-    // }
+            thread::sleep(Duration::from_millis(200));
+         }
+    }
 }
